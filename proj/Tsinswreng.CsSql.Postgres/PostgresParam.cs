a8ae594fc816dc99b76553e4f9968f4b2b3c5a2a
Tsinswreng <Tsinswreng@gmail.com>
2026-04-18 21:12:37 +0800

    改pg 參數前綴

--- proj/Tsinswreng.CsSql.Postgres/PostgresParam.cs
+++ proj/Tsinswreng.CsSql.Postgres/PostgresParam.cs
@@ -4,6 +4,6 @@ public class PostgresParamPrefix:I_AddParamPrefix{
 	protected static PostgresParamPrefix? _Inst = null;
 	public static PostgresParamPrefix Inst => _Inst??= new PostgresParamPrefix();
 	public str AddParamPrefix(str Name){
-		return ":"+Name;
+		return "@"+Name;
 	}
 }
